393783d453b7c6bffbe1c85db3a1d9cf3dc6dc62
ther208qq <thergood208@gmail.com>
2026-04-24 17:15:07 +0800

    测试

--- auth-service/src/main/java/com/jlu/auth/service/impl/UserServiceImpl.java
+++ auth-service/src/main/java/com/jlu/auth/service/impl/UserServiceImpl.java
@@ -6,7 +6,6 @@ import com.jlu.auth.service.UserService;
 import com.jlu.auth.mapper.UserMapper;
 import com.jlu.common.util.JwtTool;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
